2efef5af14edaf8b1ef4277d0dd5a76cad5a484c
sxmkkunst <kevin.kunst@siriusxm.com>
2024-03-14 16:18:20 -0500

    more code

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hello!')
+import fetch from 'node-fetch';
+
+async function main() {
+    const res = await fetch('https://httpbin.org/uuid', {
+        headers: {
+            accept: 'application/json'
+        }
+    })
+    const json = await res.json();
+    console.log(json.uuid)
+}
+
+main()
